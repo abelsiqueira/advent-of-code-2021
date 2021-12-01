4a3f0316c2c870d5d6c955d73b5c2281567c7feb
Abel Soares Siqueira <abel.s.siqueira@gmail.com>
2021-12-01 21:07:17 +0100

    Playing with optimizing day 01

--- day-01/main.jl
+++ day-01/main.jl
@@ -1,17 +1,22 @@
-using DelimitedFiles
+using DelimitedFiles, Parsers
 
-function main()
-  data = readdlm("day-01/input.txt")
-
+function main(data::Array{Int})
   # part 1
-  C = count(data[2:end] .> data[1:end-1])
-  println("C = $C")
+  n = length(data)
+  # C1 = count(data[2:end] .> data[1:end-1])
+  # C1 = count(i -> data[i+1] > data[i], 1:n-1)
+  # C1 = count(data[i+1] > data[i] for i = 1:n-1)
+  C1 = sum(data[i+1] > data[i] for i = 1:n-1)
 
   # part 2
-  n = length(data)
-  rolling = [sum(data[i:i+2]) for i = 1:n-2]
-  C = count(rolling[2:end] .> rolling[1:end-1])
-  println("C = $C")
+  # rolling = [sum(data[i:i+2]) for i = 1:n-2]
+  # C2 = count(rolling[2:end] .> rolling[1:end-1])
+  # C2 = count(data[i+3] > data[i] for i = 1:n-3)
+  # C2 = sum(data[i+3] > data[i] for i = 1:n-3)
+  C2 = count(i -> data[i+3] > data[i], 1:n-3)
+
+  C1, C2
 end
 
-main()
+data = Parsers.parse.(Int, readlines("day-01/input.txt"))
+main(data)
